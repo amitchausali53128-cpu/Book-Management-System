f9af1d4f69674ff91fffd91be761cb1f7fe53ac6
Namit210 <namitcoder16@gmail.com>
2026-08-18 13:55:56 +0530

    increased token duration

--- models/UserSchema.js
+++ models/UserSchema.js
@@ -56,7 +56,7 @@ UserSchema.methods.generateToken = async function(){
                 role: this.role
             }, 
             process.env.JWT_SECRET, 
-            { expiresIn: '1h' }
+            { expiresIn: '5h' }
         );
     }
     catch(error){
